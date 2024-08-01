f0aad459e17ee0bb613246c26bb298959746a2d8
Khurshed Alam Sifat <khurshedalamsifat@gmail.com>
2024-08-01 20:21:18 +0600

    UI update

--- GNIS_Single_Form/GNIS_Single_Form/ViewModels/MeetingDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GNIS_Single_Form.ViewModels
-{
-    public class MeetingDetail
-    {
-        public int ProductServiceId { get; set; }
-        public int Quantity { get; set; }
-        public string Unit { get; set; }
-    }
-}
--- GNIS_Single_Form/GNIS_Single_Form/ViewModels/SaveRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GNIS_Single_Form.ViewModels
-{
-    public class SaveRequest
-    {
-        public MeetingViewModel ViewModel { get; set; }
-        public string MeetingDetailsJson { get; set; }
-    }
-}
